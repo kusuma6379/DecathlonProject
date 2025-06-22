5852a25a037cc3f1bdd3efd34a89f535ccde0db0
M.kusuma <kusumababu6379@gmail.com>
2025-06-22 20:25:53 +0530

    Update ProductpageTest.java

--- ProductpageTest.java
+++ ProductpageTest.java
@@ -1,35 +1,56 @@
-package Decathlonproject;
+package DecathlonProjectTestRunner;
+
+import DecathlonProjectTest.ProductListingPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+public class ProductListingPageTest {
 
-public class ProductpageTest {
-	WebDriver driver;
-	@BeforeClass
-	void Setup() throws InterruptedException {
-		driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.decathlon.in/");
-		driver.manage().window().maximize();
-		Thread.sleep(5000);
-	}
-	@org.testng.annotations.Test
-	public void Test() {
-		ProductListingPage page=new ProductListingPage(driver);
-		page.Search("Shirts");
-		page.Shirt();
-		page.SearchBrand("NYAMBA");
-		page.BtwinProducts();
-		page.SortingSearch("500 To 1000");
-		page.Sorting();
-	}
-	@AfterClass
-	void Teardown() {
-		driver.quit();
-	}
+    WebDriver driver;
 
+    @BeforeClass
+    public void setup() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.decathlon.in/");
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testProductListingWithFilterSort() throws InterruptedException {
+        ProductListingPage listing = new ProductListingPage(driver);
+
+        listing.searchProduct("cycle");
+        Thread.sleep(3000);
+
+        listing.closePopupIfPresent();
+        Thread.sleep(4000);
+        
+        listing.applyBrandFilter();  
+        Thread.sleep(3000);
+
+        listing.sortByLowToHigh();
+        Thread.sleep(3000);
+
+        listing.printProductTitlesAndPrices();
+        Thread.sleep(3000);
+
+        listing.clickFirstProduct();
+        Thread.sleep(4000);
+
+        listing.goBackToListing(); 
+        Thread.sleep(4000);
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
 }
+
