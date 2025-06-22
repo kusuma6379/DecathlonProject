1fb9a80ecaa406910b38a52b95deeffa0020c30a
M.kusuma <kusumababu6379@gmail.com>
2025-06-22 20:17:02 +0530

    Update CartPageTest.java

--- CartPageTest.java
+++ CartPageTest.java
@@ -1,37 +1,41 @@
-package Decathlonproject;
+package DecathlonProjectTestRunner;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import DecathlonProjectTest.CartPage;
 
 public class CartPageTest {
-	WebDriver driver;
-	@BeforeClass
-	void setup() throws InterruptedException {
-		driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.decathlon.in/");
-		driver.manage().window().maximize();
-		Thread.sleep(5000);
-	}
-	@Test
-	void test() {
-		CartPage CP=new CartPage (driver);
-		CP.AddToCart();
-		CP.Cart();
-		CP.Delete();
-		CP.PopUpDelete();
-		CP.Quantity();
-		CP.Price();
-		
-	}
-	@AfterClass
-	void Teardown() {
-		driver.quit();
-	}
 
+    WebDriver driver;
+    CartPage cartPage;
+
+    @BeforeClass
+    public void setup() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.decathlon.in/");
+        Thread.sleep(3000); // Wait for initial page load
+        cartPage = new CartPage(driver);
+    }
+
+    @Test
+    public void testCartActions() {
+        cartPage.closePopupIfPresent();
+        cartPage.addToCart();
+        cartPage.openCart();
+      cartPage.deleteItem();
+        cartPage.confirmDeletePopup();
+        cartPage.adjustQuantity();
+        cartPage.checkPrice();
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
 }
+
+
